aec4f6aaf737efc62703800f91af51dfe93bfeb5
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-04-10 14:18:12 +0300

    refactored to oop

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-// ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "#FF0000";
-// ctx.fill();
-// ctx.closePath();
-
-
-
-// ctx.beginPath();
-// ctx.rect(160, 10, 100, 40);
-// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-// ctx.stroke();
-// ctx.closePath();
-
 let ball = {
-    x: 100,
-    y: 100,
-    dx: 5,
-    dy: -5,
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0,
     radius: 20,
     color: "green",
     draw() {
@@ -28,11 +14,56 @@ let ball = {
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
+        ball.move();
+    },
+    move() {
         this.x += this.dx;
         this.y += this.dy;
     },
     onWallHit: Function(),
+    onPadHit: Function(),
 }
+
+ball.onWallHit = function (wall) {
+    if (wall === "left" || wall === "right") {
+        this.dx = -this.dx;
+    }
+    else if (wall === "top") {
+        this.dy = -this.dy;
+    }
+    else if (wall === "bottom") {
+        alert("Game over");
+        game.onGameOver();        
+    }
+};
+
+ball.onPadHit = function() {
+    this.dy = -this.dy
+}
+
+let pad = {
+    x: 0,
+    y: 0,
+    height: 25,
+    width: 100,
+    color: "blue",
+    draw() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    },
+    move(e) {
+        if (game.isOn) {
+            var relativeX = e.clientX - canvas.offsetLeft;
+            if (relativeX > 0 && relativeX < canvas.width) {
+                pad.x = relativeX - pad.width / 2;
+            }
+        } 
+    }
+}
+
 
 function checkCollisions() {
     let wall;
@@ -48,72 +79,53 @@ function checkCollisions() {
     else if (ball.y + ball.radius > canvas.height) {
         wall = "bottom";
     }
-    ball.onWallHit(wall)
-}
-
-let pad = {
-    x: 0,
-    y: 0,
-    height: 20,
-    width: 100,
-    color: "blue",
-    draw() {
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.closePath();
+    ball.onWallHit(wall);
+    const COS45 = Math.cos(Math.PI / 4);
+    if (ball.x >= pad.x - ball.radius * COS45 
+        && ball.x <= pad.x + pad.width + ball.radius * COS45
+        && ball.y >= pad.y - ball.radius * COS45
+        && ball.y <= pad.y
+        )
+    {
+        ball.onPadHit();
     }
 }
-
-pad.x = (canvas.width - pad.width)/2;
-pad.y = canvas.height - pad.height;
 
 function drawAll() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.draw();
     pad.draw();
-    checkCollisions();
     requestAnimationFrame(drawAll);
 }
 
-ball.onWallHit = function (wall) {
-    if (wall === "left" || wall === "right") {
-        this.dx = -this.dx;
-    }
-    else if (wall === "top") {
-        this.dy = -this.dy;
-    }
-    else if (wall === "bottom") {
-        this.dy = -this.dy;
-        //alert("Game over");
-    }
-};
+let game = {
+    isOn: false,
+    instantiate() {
+        pad.x = (canvas.width - pad.width)/2;
+        pad.y = canvas.height - pad.height;
+        ball.x = pad.x + pad.width / 2;
+        ball.y = pad.y - ball.radius;
+        ball.dx = 0;
+        ball.dy = 0;
+    },
+    start() {
+        game.isOn = true;
+        ball.dx = 5;
+        ball.dy = -5;
+    },
+    
+    onGameOver: Function(),
+}
 
-drawAll();
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
+game.onGameOver = function() {
+    game.isOn = false;
+    this.instantiate();
+}
 
-function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    }
-    else if (e.keyCode == 37) {
-        leftPressed = true;
-    }
-}
-function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    }
-    else if (e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-        pad.x = relativeX - pad.width / 2;
-    }
-}
+document.addEventListener("mousemove", pad.move, false);
+document.addEventListener("keypress", game.start, false);
+requestAnimationFrame(drawAll)
+setInterval(checkCollisions, 50);
+game.instantiate();
+
+
